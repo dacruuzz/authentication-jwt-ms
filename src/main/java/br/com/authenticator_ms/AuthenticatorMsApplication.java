c0684d8b1a2e5c580457c79001b45d40c506796a
dacruuzz <gustavo.paulino.cantanhede@gmail.com>
2026-09-17 14:56:25 -0300

    first commit

---
+++ src/main/java/br/com/authenticator_ms/AuthenticatorMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.authenticator_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatorMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/authenticator_ms/AuthenticatorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.authenticator_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
